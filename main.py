5e4b489ba3a6429f54f19f90191aafa5f70d4e99
algorit444 <eld-m2008@rambler.ru>
2026-04-25 12:28:53 +0300

    Добавление инициализации игры

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
+from pygame import *
 
+display.set_caption("Ping Pong")
+window = display.set_mode((480, 360))
+
+timer = time.Clock()
+game = True
+while game:
+    for e in event.get():
+        if e.type == QUIT:
+            game = False
+    timer.tick(60)
