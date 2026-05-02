b7631bb21cf82c8a207fb2c4d2223c328dcb2a4c
algorit444 <eld-m2008@rambler.ru>
2026-05-02 11:38:17 +0300

    обновление main.py
    добавил систему счёта, изменил скорость мяча

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from pygame import *
-
 display.set_caption("Ping Pong")
 window = display.set_mode((800, 600))
 
@@ -7,6 +6,7 @@ font.init()
 font1 = font.SysFont('Arial', 80)
 win1 = font1.render('Player 1 won!', True, (255, 0, 0))
 win2 = font1.render('Player 2 won!', True, (0, 0, 255))
+font2 = font.SysFont('Arial', 60)
 
 class GameSprite(sprite.Sprite):
     def __init__(self, player_image, player_x, player_y, size_x, size_y, player_speed):
@@ -36,9 +36,11 @@ class Player(GameSprite):
 racket1 = Player("racket.png", 15, 250, 25, 100, 4)
 racket2 = Player("racket.png", 765, 250, 25, 100, 4)
 ball = GameSprite("tenis_ball.png", 400, 270, 30, 30, 0)
-b_sx = 2
-b_sy = -2
+b_sx = 3
+b_sy = -5
 timer = time.Clock()
+score1 = 0
+score2 = 0
 game = True
 finish = True
 while game:
@@ -52,15 +54,25 @@ while game:
         ball.rect.y+=b_sy
         if ball.rect.y<0 or ball.rect.y>570:
             b_sy*=-1
-
         if ball.rect.colliderect(racket1.rect) or ball.rect.colliderect(racket2.rect):
             b_sx*=-1
         if ball.rect.x>800:
-            window.blit(win1, (200, 250))
-            finish = False
+            score1+=1
+            ball.rect.x, ball.rect.y = 400,270
+            if score1 > 4:
+                window.blit(win1, (200, 250))
+                finish = False
         elif ball.rect.x<0:
-            window.blit(win2, (200, 250)) 
-            finish = False
+            score2+=1
+            ball.rect.x, ball.rect.y = 400,270
+            if score2 > 4:
+                window.blit(win2, (200, 250)) 
+                finish = False
+        scoretxt1 = font2.render(str(score1), True, (0, 0, 0))
+        scoretxt2 = font2.render(str(score2), True, (0, 0, 0))
+        window.blit(scoretxt1, (100, 50))
+        window.blit(scoretxt2, (700, 50))
+
         racket1.update_l()
         racket1.reset()
         racket2.update_r()
